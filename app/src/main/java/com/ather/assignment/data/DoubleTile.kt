5e371b851b910fa63a672fb741e618f4a6435211
Asutosh Panda <panda.asutosh04@gmail.com>
2021-10-03 12:44:29 +0530

    Delete unused data class

--- app/src/main/java/com/ather/assignment/data/DoubleTile.kt
+++
@@ -1,6 +0,0 @@
-package com.ather.assignment.data
-
-import android.widget.TextView
-
-data class DoubleTile(val view1Position: Int, val numberInString1: String,
-                      val view2Position: Int, val numberInString2: String)
